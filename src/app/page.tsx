dce74d538ad4489206c7e7a07f5c9799356cb2b0
taga-dysong <80451567+bkbros@users.noreply.github.com>
2025-06-17 11:22:10 +0900

    권한 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     const mallId = process.env.NEXT_PUBLIC_CAFE24_MALL_ID!;
     const clientId = process.env.NEXT_PUBLIC_CAFE24_CLIENT_ID!;
     const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_BASE_URL}/oauth/callback`);
-    const scope = encodeURIComponent("mall.read_product,product");
+    const scope = encodeURIComponent("mall.read_product");
     const state = "admin_access";
     const url = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
     window.location.href = url;
