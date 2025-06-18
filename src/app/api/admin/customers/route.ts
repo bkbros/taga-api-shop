3b5d0f50e2022d4e37a0171624a0963b97435720
taga-dysong <80451567+bkbros@users.noreply.github.com>
2025-06-18 17:32:22 +0900

    회원조회 api 수정

--- src/app/api/admin/customers/route.ts
+++ src/app/api/admin/customers/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: Request) {
 
     const mallId = process.env.NEXT_PUBLIC_CAFE24_MALL_ID!;
     const apiVer = process.env.CAFE24_API_VERSION!;
-    const userId = "sda0125";
+
     // 3) Admin API 호출
     const response = await axios.get(`https://${mallId}.cafe24api.com/api/v2/admin/customers`, {
       headers: {
