bb14e78183236b875e569c285318f3dfa591de4b
taga-dysong <80451567+bkbros@users.noreply.github.com>
2025-06-24 16:31:19 +0900

    회원조회 변수 수정

--- src/app/api/admin/customers/route.ts
+++ src/app/api/admin/customers/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
 
     // 파라미터
 
-    const memberId = "sda0125";
+    const memberId = "010-3339-8058";
 
     if (!memberId) {
       return NextResponse.json({ error: "member_id 파라미터가 필요합니다" }, { status: 400 });
